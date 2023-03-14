8d12386b1a95275030670d6dfc6ed9e1148a31f4
Cody Seibert <cseibert113@gmail.com>
2023-03-14 17:10:40 -0400

    displaying the table and form

--- app.vue
+++ app.vue
@@ -1,9 +1,28 @@
+<script setup>
+const entries = [
+  {
+    npi: '123427482938',
+    timeSubmitted: '2011-10-05T14:48:00.000Z',
+  },
+  {
+    npi: '123427482938',
+    timeSubmitted: '2011-10-05T14:48:00.000Z',
+  },
+  {
+    npi: '123427482938',
+    timeSubmitted: '2011-10-05T14:48:00.000Z',
+  },
+];
+</script>
+
 <template>
+  <h1>Dental Claim Entry</h1>
   <form>
-    <label class="usa-label npi-form-label" for="input-type-text"
-      >National Provider Identifier (NPI)</label
-    >
-    <span class="hint">For example "1234567890"</span>
+    <label class="usa-label text-bold" for="input-type-text">
+      National Provider Identifier (NPI)<br />
+      <span class="text-normal">Item 49 - Form XX</span>
+    </label>
+    <span class="text-italic">For example "1234567890"</span>
 
     <input
       class="usa-input"
@@ -12,17 +31,26 @@
       id="input-type-text"
       name="input-type-text"
     />
-
+    <div>Claim Submission Successful</div>
     <button type="submit" class="usa-button">Submit</button>
   </form>
+
+  <hr />
+
+  <h2>Submitted Claim history</h2>
+
+  <table class="usa-table">
+    <thead>
+      <tr>
+        <th>NPI</th>
+        <th>Time Submitted</th>
+      </tr>
+    </thead>
+    <tr v-for="entry in entries">
+      <th>{{ entry.npi }}</th>
+      <td>{{ entry.timeSubmitted }}</td>
+    </tr>
+  </table>
 </template>
 
-<style>
-.npi-form-label {
-  font-weight: bold;
-}
-
-.hint {
-  font-style: italic;
-}
-</style>
+<style scoped></style>
