bcc9af6fc53196c790f62768d91fcd82a26c3b1a
Rachel Schneiderman <rschneiderman379@gmail.com>
2023-03-15 10:02:00 -0700

    try to submit form in test with invalid values

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,8 +1,20 @@
-import { mount } from '@vue/test-utils';
-import { test } from 'vitest';
-import Component from '../pages/index.vue';
+import { render, fireEvent } from "@testing-library/vue";
+import { test } from "vitest";
+import Component from "../pages/index.vue";
+import { VueQueryPlugin } from "@tanstack/vue-query";
 
-test('increments value on click', async () => {
-  // TODO: setup vue query plugin somehow in tests
-  const wrapper = mount(Component);
+test("increments value on click", async () => {
+  const wrapper = render(Component, {
+    global: {
+      plugins: [VueQueryPlugin],
+    },
+  });
+
+  const input = wrapper.getByLabelText(/National Provider Identifier/);
+
+  await fireEvent.update(input, "123456789");
+
+  const submitButton = wrapper.getByText("Submit");
+
+  await fireEvent.click(submitButton);
 });
